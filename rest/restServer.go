0037479cc623777966d0993b4607ea54f4efd146
laser-wang <mnbvc94449@163.com>
2017-01-05 23:08:22 +0800

    1

--- rest/restServer.go
+++ rest/restServer.go
@@ -10,7 +10,7 @@ import (
 	"net/http"
 	//	"os"
 	//	"runtime"
-	//	"time"
+	"time"
 
 	"database/sql"
 	//数据库连接池
@@ -26,11 +26,11 @@ import (
 )
 
 var (
-	redisClient *redis.Pool
-	redis_host  string
-	redis_db    int
+	gRedisClient *redis.Pool
+	gRedisHost   string
+	gRedisDB     int
 
-	restDB *sql.DB
+	gRestDB *sql.DB
 )
 
 type structResult struct {
@@ -43,10 +43,39 @@ func init() {
 
 	var err error
 
+	//初始化日志
 	l4g.AddFilter("stdout", l4g.DEBUG, l4g.NewConsoleLogWriter())
 	l4g.AddFilter("file", l4g.DEBUG, l4g.NewFileLogWriter("rest.log", false))
-	restDB, err = sql.Open("postgres", "postgres://avcp_work:avcp_work@192.168.0.10/avcp_work?sslmode=disable")
+
+	// 初始化数据库,已自带连接池
+	gRestDB, err = sql.Open("postgres", "postgres://avcp_work:avcp_work@192.168.0.10/avcp_work?sslmode=disable")
+	// 连接的最大空闲时间(可选)
+	gRestDB.SetConnMaxLifetime(300 * time.Second)
+	// 默认不限制
+	gRestDB.SetMaxOpenConns(2000)
+	// 默认为0
+	gRestDB.SetMaxIdleConns(100)
 	checkErr(err, check_flag_exit)
+
+	// 初始化redis
+	gRedisHost = "192.168.0.10:6379"
+	gRedisDB = 1
+	// 建立连接池
+	gRedisClient = &redis.Pool{
+		// 从配置文件获取maxidle以及maxactive，取不到则用后面的默认值
+		MaxIdle:     10,
+		MaxActive:   100,
+		IdleTimeout: 180 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", gRedisHost)
+			if err != nil {
+				return nil, err
+			}
+			// 选择db
+			c.Do("SELECT", gRedisDB)
+			return c, nil
+		},
+	}
 
 }
 
@@ -54,8 +83,8 @@ func init() {
 func StartServer() {
 
 	// 连接数
-	restDB.SetMaxOpenConns(100) // 最大连接数
-	restDB.SetMaxIdleConns(20)  // 最大空闲数
+	gRestDB.SetMaxOpenConns(100) // 最大连接数
+	gRestDB.SetMaxIdleConns(20)  // 最大空闲数
 
 	startHttp()
 	defer releaseResource()
@@ -77,9 +106,9 @@ func login(w http.ResponseWriter, req *http.Request) {
 
 	var err error
 	//查询数据
-	rows, err := restDB.Query("SELECT user_name FROM xx_user where user_code = $1 and pwd = $2", userCode, pwd)
+	rows, err := gRestDB.Query("SELECT user_name FROM xx_user where user_code = $1 and pwd = $2", userCode, pwd)
 
-	//	rows, err := restDB.Query("SELECT user_name FROM xx_user")
+	//	rows, err := gRestDB.Query("SELECT user_name FROM xx_user")
 
 	checkErr(err, check_flag_log)
 
@@ -151,7 +180,8 @@ func startHttp() {
 //释放日志文件、数据库连接等资源
 func releaseResource() {
 	l4g.Close()
-	restDB.Close()
+	gRestDB.Close()
+	gRedisClient.Close()
 }
 
 //func ListenAndServe(addr string, handler http.Handler, timeout time.Duration) error {
@@ -180,3 +210,8 @@ func checkErr(err error, flag int) {
 	}
 
 }
+
+// setToken 登陆后设置token
+//func setToken(String userCode) {
+
+//}
